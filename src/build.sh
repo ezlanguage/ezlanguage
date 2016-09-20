e81ac314f2f32af2f21a08d8e932f7e5619a8ddc
flodavid <fl.david.53@gmail.com>
2016-09-20 19:32:08 +0200

    fichier build avec architecture + debut correction makefile, à voir pour le rendre fonctionnel

--- src/build.sh
+++ src/build.sh
@@ -5,13 +5,13 @@ PROGRAM_NAME="EZ_language_compiler"
 
 if [ -z "$1" ]
 then
-    echo "Please, enter a flex filename in argument"
+    echo "Please, enter a yacc filename in argument"
 else
     echo "bison $1"
     bison -d 	$1
     echo "flex $PROGRAM_NAME.lex"
     flex	$PROGRAM_NAME".lex"
-    g++ -c	lex.yy.c	-o lex.yy.o
-    g++ -c	$PROGRAM_NAME".tab.c"	-o $PROGRAM_NAME".tab.o"
-    g++	-o	EZL_compiler	"lex.yy.o"	$PROGRAM_NAME".tab.o"  -lm -ll -lfl
+    g++ -c	lex.yy.c	-o ../obj/lex.yy.o
+    g++ -c	$PROGRAM_NAME".tab.cpp"	-o ../obj/$PROGRAM_NAME".tab.o"
+    g++	-o	../EZL_compiler	../obj/lex.yy.o	../obj/$PROGRAM_NAME".tab.o" -lm -ll -lfl
 fi
