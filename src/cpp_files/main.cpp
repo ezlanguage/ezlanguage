999bb28eaafa946bbfa59cc83e61c30cdc0f8eb2
Florentin <flo.noel@hotmail.fr>
2016-09-21 11:21:38 +0200

    test

--- src/cpp_files/main.cpp
+++ src/cpp_files/main.cpp
@@ -6,7 +6,7 @@ int main ( int argc , char ** argv )
 {
         
     if(argc > 1) {
-	if(argc ==2) {
+        if(argc ==2) {
 	    yyin = fopen(argv[1], "r");
 	    cout << "Utilisation d'un fichier"<< endl;
 	}
