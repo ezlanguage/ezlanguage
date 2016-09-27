bf2cfd9eabdd710bcd9a38e0bee2eff98ed79974
Florentin <flo.noel@hotmail.fr>
2016-09-27 16:55:09 +0200

    Ajout de fichiers basiques

--- src/cpp_files/global.h
+++ src/cpp_files/global.h
@@ -3,12 +3,12 @@
 
 //#include "MyClasses/maclasse.h"
 
-typedef struct s_mon_type{
-    int		valeur_numerique;
-    bool 	booleen;
-    char*	texte;
+typedef struct s_mon_type {
+    int valeur_numerique;
+    bool booleen;
+    char *texte;
 //    MaClasse	une_maclasse;
-}mon_type;
+} mon_type;
 
 #define YYSTYPE mon_type
 extern YYSTYPE yylval;
--- src/cpp_files/main.cpp
+++ src/cpp_files/main.cpp
@@ -2,14 +2,12 @@
 
 using namespace std;
 
-int main ( int argc , char ** argv )
-{
-        
-    if(argc > 1) {
-        if(argc ==2) {
-	    yyin = fopen(argv[1], "r");
-	    cout << "Utilisation d'un fichier"<< endl;
-	}
-	else cerr << "Trop d'arguments en paramètre, ceux en trop sont ignorés"<< endl;
+int main(int argc, char **argv) {
+
+    if (argc > 1) {
+        if (argc == 2) {
+            yyin = fopen(argv[1], "r");
+            cout << "Utilisation d'un fichier" << endl;
+        } else cerr << "Trop d'arguments en paramètre, ceux en trop sont ignorés" << endl;
     }
 }
