c2b119c2bed8bd2f3776ce3b80f365fb684d98f6
ROUINEB Hamza <phoenix@rouinebs-macbook-pro.local>
2016-11-27 22:42:32 +0100

    changing the destructor of node's class

--- src/modules/Node.h
+++ src/modules/Node.h
@@ -7,16 +7,20 @@
 using namespace std;
 
 //Basic class, used to structure the others classes as nodes of the main tree
-class Node{
+class Node {
 protected:
     string name;
-    Node* left_son;
-    Node* right_son;
+    Node *left_son;
+    Node *right_son;
 
 public:
-    Node(){};
+    Node() {};
+
     virtual string translate() =0; // this function will allow the translation of the nodes's tree into c++ instructions
-    ~Node(){};
+    ~Node() {
+        delete left_son;
+        delete right_son;
+    };
 };
 
 #endif //NODE_H
