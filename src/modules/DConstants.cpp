2d8d79d2d7744ac7ed2efff50505d52bfc90fd7e
ROUINEB <rouineb.work@gmail.com>
2017-01-12 11:16:05 +0100

    Adding missing constructor

--- src/modules/DConstants.cpp
+++ src/modules/DConstants.cpp
@@ -8,6 +8,10 @@ DConstants::DConstants() : Holder("", Holder::TYPES::CONST) {}
 
 DConstants::DConstants(const string &name) : Holder(name, Holder::TYPES::CONST) {}
 
+DConstants::DConstants(const string &name, DTypes *son) : Holder(name, Holder::TYPES::CONST) {
+    this->set_right_son(son);
+}
+
 string DConstants::translate() {
     // same thing : not yet implemented
     return "";
