7da4ce52ad7f79bc3a401aaa6c7af1b2264349c3
zakaria22 <lahyani.zakaria.lz@gmail.com>
2017-02-03 18:06:50 +0100

    Declaration Conteneurs

---
+++ src/modules/DeclarationMap.h
@@ -0,0 +1,127 @@
+#ifndef DECLARATIONMAP_H
+#define DECLARATIONMAP_H
+
+
+#include <vector>
+#include <iostream>
+#include "Node.h"
+#include "../hash_table/Variable.h"
+
+/**
+ * @class DeclarationList
+ * @brief Class representing list's data structer declarations
+ * 
+ * @author LAHYANI Zakaria
+ */
+
+class DeclarationMap : public Node
+{
+    public:
+
+
+    	/* * * * * * * * *
+		* CONSTRUCTORS  *
+		* * * * * * * * */
+		
+
+    	/**
+		* Minimal constructor Takes the container type & key, and name (its name)
+		* 
+		* @param : variable 
+		*
+		* @author LAHYANI Zakaria
+		*/
+        DeclarationMap(Variable *,std::string, std::string) ;
+
+		DeclarationMap(Variable *v, std::string, std::string, std::vector<std::string>, std::vector<std::string> );
+        /**
+		* Copy COnstructor 
+		*  
+		* @param : container Container 
+		* 
+		* @author LAHYANI Zakaria
+		*/
+		DeclarationMap(const DeclarationMap& );
+
+		/**
+		* Destructor
+		* @author LAHYANI Zakaria
+		*/
+        virtual ~DeclarationMap();
+
+
+
+        /* * * * * * * * * * * * * *
+		* ACCESSORS  AND MUTATORS *
+		* * * * * * * * * * * * * */
+
+        /**
+		* getter of Variable
+		*
+		* @author LAHYANI Zakaria
+		*/
+		Variable * get_variable() const;
+
+		/**
+		* get keys type
+		*
+		* @author LAHYANI Zakaria
+		*/
+		std::string get_key_type() const;
+
+
+		/**
+		* get value type
+		*
+		* @author LAHYANI Zakaria
+		*/
+		std::string get_value_type() const;
+
+
+ 		/**
+		* get keys
+		*
+		* @author LAHYANI Zakaria
+		*/
+		std::vector<std::string> get_key() const;
+	
+ 		/**
+		* get values
+		*
+		* @author LAHYANI Zakaria
+		*/
+		std::vector<std::string> get_value() const;
+
+		/**
+		* containerter of variable
+		*
+		* @param v : the Variable of the container
+		* @author LAHYANI Zakaria
+		*/
+		void set_variable(Variable *);
+
+
+
+		/* * * * * * * *
+		* Translation  *
+		* * * * * * * **/
+		
+
+		/**
+		 * @brief Translation of the instruction into it's C++ counterpart
+		 * @return a string containing the C++ code of the declaration
+		 * 
+		 * The instance will be translated with it's C++ equivalent using its informations
+		 * The translation corresponds to its specifications, specificities and values
+		 */
+		virtual std::string preTranslate() const;
+
+	private:
+		Variable *var;
+		std::string key_type;
+		std::string value_type;
+    	std::vector<std::string> keys;
+    	std::vector<std::string> values;
+};
+
+#endif // DECLARATIONMAP_H
---
+++ src/modules/DeclarationVector.h
@@ -0,0 +1,83 @@
+#ifndef DECLARATIONVECTOR_H
+#define DECLARATIONVECTOR_H
+
+#include <vector>
+#include <iostream>
+#include "Node.h"
+#include "../hash_table/Variable.h"
+#include "DeclarationContainer.h"
+
+/**
+ * @class DeclarationVector
+ * @brief Class representing vector's data structer declarations
+ * 
+ * @author LAHYANI Zakaria
+ */
+
+class DeclarationVector : public Node , DeclarationContainer
+{
+    public:
+		
+
+		/* * * * * * * * *
+		* CONSTRUCTORS  *
+		* * * * * * * * */
+		
+
+    	/**
+		* Minimal constructor Takes the container type, and name (its name)
+		* 
+		* @param : variable 
+		*
+		* @author LAHYANI Zakaria
+		*/
+        DeclarationVector(Variable *);
+
+		/**
+		* Constructor Takes the container variable, name and its size
+		* 
+		* @param : variable 
+		* @param : size
+		*
+		* @author LAHYANI Zakaria
+		*/
+        DeclarationVector(Variable *,  int);
+
+        /**
+		* Constructor Takes the container variable, name and its initial values
+		* 
+		* @param : variable 
+		* @param : intial values
+		*
+		* @author LAHYANI Zakaria
+		*/
+        DeclarationVector(Variable *, std::vector<std::string>);
+
+        /**
+		* Copy COnstructor 
+		*  
+		* @param : container Container 
+		* 
+		* @author LAHYANI Zakaria
+		*/
+		DeclarationVector(const DeclarationVector& );
+
+
+		/* * * * * * * *
+		* Translation  *
+		* * * * * * * **/
+		
+
+		/**
+		 * @brief Translation of the instruction into it's C++ counterpart
+		 * @return a string containing the C++ code of the declaration
+		 * 
+		 * The instance will be translated with it's C++ equivalent using its informations
+		 * The translation corresponds to its specifications, specificities and values
+		 */
+		virtual std::string preTranslate() const;
+
+
+};
+
+#endif // DECLARATIONVECTOR_H
