e49d6a44aea18cd7def61ebac1aa3f46603356e8
Valentin Ginisty <valentin.ginisty@gmail.com>
2017-02-08 11:14:06 +0100

    methode d'expression conditionnelle + ajout commentaire

--- src/modules/ExpressionConditionnelle.cpp
+++ src/modules/ExpressionConditionnelle.cpp
@@ -4,19 +4,6 @@
 
 using namespace std;
 
-/*
- * Différents cas à traiter
- * cas d'une variable
- * cas d'un booléen
- * cas d'une fonction
- * cas d'une comparaison (avec opérateur)
- * les différentes conditions bout à bout
- */
-
-/* une condition est un ensemble d'expressions entrecoupées par des opérateurs logiques
- * Il faut donc split la chaîne de conditions sur chaque opérateur
- * Traduire chaque opérateur et traduire ce qu'il se trouve à droite et à gauche de chaque opérateur
- */
 ExpressionConditionnelle::ExpressionConditionnelle(): operateur(""), valeur(""), left_part(nullptr), right_part(nullptr){
 
 }
--- src/modules/ExpressionConditionnelle.h
+++ src/modules/ExpressionConditionnelle.h
@@ -20,10 +20,39 @@ private:
 protected:
 
 public:
+    /**
+     * @brief Constructor
+     */
     ExpressionConditionnelle();
+
+    /**
+     * @brief Constructor
+     * @param operateur_unaire : unary operator (with one parameter)
+     * @param operande : conditionnal expression
+     */
     ExpressionConditionnelle(std::string operateur_unaire, const ExpressionConditionnelle* operande);
+
+    /**
+     * @brief Constructor
+     * @param operateur_unaire : binary operator (with two parameters)
+     * @param left_operande : conditionnal expression which is the first operand
+     * @param right_operande : conditionnal expression which is the second operand
+     */
     ExpressionConditionnelle(std::string operateur_binaire, const ExpressionConditionnelle* left_operande, const ExpressionConditionnelle* right_operande);
+
+    /**
+     * @brief Constructor
+     * @param operande : value of the expression
+     */
     ExpressionConditionnelle(std::string operande);
+
+    /**
+     * @brief Translate the begining part of the Forall
+     * @return a string containing the C++ code of the node
+     *
+     * All subclasses, should reimplement this method so that the translation corresponds
+     * to their specifications, specificities and own values
+     */
 
     virtual std::string preTranslate() const;
 };
